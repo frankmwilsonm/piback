da088656d9d9b5f707ccd53237b6de4b95328175
frankmwilsonm <frank.mamani@upeu.edu.pe>
2022-10-24 05:48:35 -0500

    stable version /gitu-2

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const tasksRouter = require('./tasks.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-  router.use('/products', productsRouter);
+  router.use('/proyectos', productsRouter); // /products
   router.use('/tasks', tasksRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
--- routes/tasks.router.js
+++ routes/tasks.router.js
@@ -1,15 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
-
-// const TasksService = require('./../services/tasks.service');
-
-// const service = new TasksService();
-
 router.get('/', (req, res) => {
-    const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+  res.send("hollo ");
+  
 });
 
 module.exports = router;
